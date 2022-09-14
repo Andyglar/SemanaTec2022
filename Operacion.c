07f0d241b2b68220c5e9ca003c6917efa66d30e3
Fabián Erubiel Rojas Yañez <a01706636@itesm.mx>
2022-09-14 18:18:19 -0500

    Funciones anadidas

--- Operacion.c
+++ Operacion.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+int suma(int a, int b){
+ return a+b;
+}
+int resta(int a, int b){
+ return a-b;
+}
+
 int main(){
 int num1 = 12;
 int num2 = 3;
@@ -12,3 +20,5 @@ int op3 = multi(num1,num2);
 printf("%i",op3);
 return 0;
 }
+
+
